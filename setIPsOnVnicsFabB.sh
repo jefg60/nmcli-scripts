2e185f7dce84324c7bbb5af8636faa8ad04d04e3
Jeff Hibberd <jeff@jeffhibberd.com>
2019-07-12 17:52:22 +0100

    add ip get func

--- setIPsOnVnicsFabB.sh
+++ setIPsOnVnicsFabB.sh
@@ -3,8 +3,14 @@ function get_mac {
   nmcli -f general.hwaddr device show $1 | awk '{print $NF}'
 }
 
-eth0_mac=$(get_mac eth0)
-eth1_mac=$(get_mac eth1)
+function get_ip {
+  nmcli -f ip4.address device show $1 | awk '{print $NF}'
+}
 
-echo eth0: $eth0_mac
-echo eth1: $eth1_mac
+eth0_mac=$(get_mac eth0)
+eth0_ip=$(get_ip eth0)
+eth1_mac=$(get_mac eth1)
+eth1_ip=$(get_ip eth1)
+
+echo eth0: mac: $eth0_mac ip: $eth0_ip
+echo eth1: $eth1_mac ip: $eth1_ip
